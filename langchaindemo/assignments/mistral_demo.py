a8f9c0a27b9925b830d4c35179dd288a3c6ce373
mohankumarn24 <mohankumar.n24@outlook.com>
2026-05-08 19:50:13 +0530

    refactor

--- langchaindemo/assignments/mistral_demo.py
+++
@@ -1,8 +0,0 @@
-import os
-from langchain_community.chat_models import ChatOllama
-
-llm=ChatOllama(model="mistral")
-
-question = input("Enter the question")
-response = llm.invoke(question)
-print(response.content)
--- langchaindemo/basics/gemma_demo.py
+++
@@ -1,7 +0,0 @@
-from langchain_community.chat_models import ChatOllama
-
-llm=ChatOllama(model="gemma:2b")
-
-question = input("Enter the question")
-response = llm.invoke(question)
-print(response.content)
